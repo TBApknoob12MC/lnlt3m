106e1e06dfdf359d14be8c3aad180f84fff8d28d
TBApknoob12MC <apknoobgpro@gmail.com>
2026-05-16 09:08:32 +0530

    update

--- lnlt3m.lua
+++ lnlt3m.lua
@@ -28,7 +28,7 @@ function model:think(st)
   return m_posnum
 end
 
-local function clrscr() if os.execute("clear") ~= 0 then os.execute("cls") end end
+local function clrscr() if not os.execute("clear") then os.execute("cls") end end
 
 local function checkwinner(st)
   local wins = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7} }
